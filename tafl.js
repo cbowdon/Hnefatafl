4eca707dbd18947b45cd9997c5d2b7f69512aa55
Chris Bowdon <c.bowdon@bath.edu>
2014-01-26 18:15:47 +0000

    Refactored for node AND browser use, added board cell array.

--- tafl.js
+++ tafl.js
@@ -1,133 +1,209 @@
-"use strict";
+(function (root) {
+    "use strict";
 
-// Immutable Move object
-var Move = (function () {
-    var that = {};
+    var Cell, Move, Board, types, team;
 
-    function Move(arg) {
-        if (!arg.player) { throw new TypeError("'player' not defined"); }
-        if (!arg.from || arg.from.length !== 2) { throw new TypeError("'from' not a 2-element array"); }
-        if (!arg.to || arg.to.length !== 2) { throw new TypeError("'to' not a 2-element array"); }
-
-        that.player = arg.player;
-        that.start = arg.from;
-        that.end = arg.to;
-    }
-
-    Move.prototype = {
-        get player() { return that.player; },
-        set player(value) { throw new Error(); },
-
-        get start() { return that.start; },
-        set start(value) { throw new Error(); },
-
-        get end() { return that.end; },
-        set end(value) { throw new Error(); },
-
-        get horizontal() { return that.start[0] === that.end[0]; },
-        set horizontal(value) { throw new Error(); },
-
-        get vertical() { return that.start[1] === that.end[1]; },
-        set vertical(value) { throw new Error(); },
-
-        get distance() {
-            if (this.horizontal) {
-                return Math.abs(that.start[1] - that.end[1]);
-            }
-            else if (this.vertical) {
-                return Math.abs(that.start[0] - that.end[0]);
-            }
-            throw new TypeError("Invalid (diagonal) move coordinates.");
-        },
-        set distance(value) { throw new Error(); },
-    };
-    return Move;
-}());
-
-module.exports.Board = (function BoardClosure() {
-    function defaultState() {
-        return [
-            "E--aaaaa--E",
-            "-----a-----",
-            "-----------",
-            "a----d----a",
-            "a---ddd---a",
-            "aa-ddkdd-aa",
-            "a---ddd---a",
-            "a----d----a",
-            "-----------",
-            "-----a-----",
-            "E--aaaaa--E",
-        ];
-    }
-
-    var that;
-
-    function Board(arg) {
-        that = arg || {
-            state: defaultState(),
-            activePlayer: "defenders", // could also be "attackers"
-            moves: []
-        };
-    }
-
-    Board.prototype = {
-
-        // TODO  state should be computed
-        get state() { return that.state; },
-        set state(value) { throw new Error(); },
-
-        get sideLength() { return that.state.length; },
-        set sideLength(value) { throw new Error(); },
-
-        get activePlayer() { return that.activePlayer; },
-        set activePlayer(value) { throw new Error(); },
-
-        clear: function (move) {
+    types = Object.freeze({
+        king: "k",
+        attacker: "a",
+        defender: "d",
+        safehouse: "S",
+        none: "-",
+        lookup: function (value) {
+            Object.keys(this).forEach(function (key) {
+                if (this[key] === value) {
+                    return key;
+                }
+            });
             return false;
         },
+    });
 
-        occupant: function (cell) {
-            return "defenders";
-        },
+    team = Object.freeze({
+        attackers: "attackers",
+        defenders: "defenders",
+    });
 
-        update: function (arg) {
-            var move = new Move(arg),
-                errMsg = this.invalid(move);
+    // Cell with mutable occupier
+    Cell = (function CellClosure() {
+        var that;
 
-            if (errMsg) {
-                throw new Error("Invalid move: " + errMsg);
-            }
-
-            that.moves.push(move);
-        },
-
-        invalid: function (move) {
-            if (move.player !== this.activePlayer) {
-                return "wrong player";
-            }
-            if (move.start[0] >= this.sideLength
-                    || move.end[0] >= this.sideLength
-                    || move.start[1] >= this.sideLength
-                    || move.end[1] >= this.sideLength) {
-                return "outside board";
-            }
-            if (this.occupant(move.start) !== move.player) {
-                return "player's piece not at start";
-            }
-            if (this.occupant(move.end) !== "none") {
-                return "end position olready occuppied";
-            }
-            if (!move.horizontal && !move.vertical) {
-                return "not straight";
-            }
-            if (!this.clear(move)) {
-                return "path blocked";
-            }
-            return false;
+        function Cell(arg) {
+            that = {
+                row: arg.row,
+                col: arg.col,
+                name: "R" + arg.row + "C" + arg.col,
+                occupier: arg.occupier || types.none,
+            };
         }
 
-    };
+        Cell.prototype = {
 
-    return Board;
-}());
+            get row() { return that.row; },
+            set row(value) { throw new TypeError(); },
+
+            get col() { return that.col; },
+            set col(value) { throw new TypeError(); },
+
+            get name() { return that.name; },
+            set name(value) { throw new TypeError(); },
+
+            get occupier() { return that.occupier; },
+            set occupier(value) {
+                if (that.occupier !== types.none) {
+                    throw new Error("Cell already occupied");
+                }
+                that.occupier = value;
+            },
+        };
+
+        return Cell;
+    }());
+
+    // Immutable Move object
+    Move = (function MoveClosure() {
+        var that = {};
+
+        function Move(arg) {
+            if (!arg.player) { throw new TypeError("'player' not defined"); }
+            if (!arg.from || arg.from.length !== 2) { throw new TypeError("'from' not a 2-element array"); }
+            if (!arg.to || arg.to.length !== 2) { throw new TypeError("'to' not a 2-element array"); }
+
+            that.player = arg.player;
+            that.start = arg.from;
+            that.end = arg.to;
+        }
+
+        Move.prototype = {
+            get player() { return that.player; },
+            set player(value) { throw new TypeError(); },
+
+            get start() { return that.start; },
+            set start(value) { throw new TypeError(); },
+
+            get end() { return that.end; },
+            set end(value) { throw new TypeError(); },
+
+            get horizontal() { return that.start[0] === that.end[0]; },
+            set horizontal(value) { throw new TypeError(); },
+
+            get vertical() { return that.start[1] === that.end[1]; },
+            set vertical(value) { throw new TypeError(); },
+
+            get distance() {
+                if (this.horizontal) {
+                    return Math.abs(that.start[1] - that.end[1]);
+                }
+                if (this.vertical) {
+                    return Math.abs(that.start[0] - that.end[0]);
+                }
+                throw new TypeError("Invalid (diagonal) move coordinates.");
+            },
+            set distance(value) { throw new TypeError(); },
+        };
+        return Move;
+    }());
+
+    Board = (function BoardClosure() {
+        var that,
+            defaultState;
+
+        defaultState = [
+            ["S", "-", "-", "a", "a", "a", "a", "a", "-", "-", "S"],
+            ["-", "-", "-", "-", "-", "a", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["a", "-", "-", "-", "-", "d", "-", "-", "-", "-", "a"],
+            ["a", "-", "-", "-", "d", "d", "d", "-", "-", "-", "a"],
+            ["a", "a", "-", "d", "d", "k", "d", "d", "-", "a", "a"],
+            ["a", "-", "-", "-", "d", "d", "d", "-", "-", "-", "a"],
+            ["a", "-", "-", "-", "-", "d", "-", "-", "-", "-", "a"],
+            ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "a", "-", "-", "-", "-", "-"],
+            ["S", "-", "-", "a", "a", "a", "a", "a", "-", "-", "S"]
+        ];
+
+        function buildBoard(state) {
+            var board = state.map(function (row, i) {
+                row.map(function (cellValue, j) {
+                    var type = types.lookup(cellValue);
+                    return new Cell({ occupier: type, row: i, col: j });
+                });
+            });
+        }
+
+        function Board(arg) {
+            that = arg || {
+                state: defaultState,
+                activePlayer: team.defenders, // could also be team.attackers
+                moves: []
+            };
+
+            that.board = buildBoard(that.state);
+        }
+
+        Board.prototype = {
+
+            // TODO  state should be computed
+            get state() { return that.state; },
+            set state(value) { throw new TypeError(); },
+
+            get sideLength() { return that.state.length; },
+            set sideLength(value) { throw new TypeError(); },
+
+            get activePlayer() { return that.activePlayer; },
+            set activePlayer(value) { throw new TypeError(); },
+
+            clear: function (move) {
+                return false;
+            },
+
+            occupant: function (cell) {
+                return team.defenders;
+            },
+
+            update: function (arg) {
+                var move = new Move(arg),
+                    errMsg = this.invalid(move);
+
+                if (errMsg) {
+                    throw new Error("Invalid move: " + errMsg);
+                }
+
+                that.moves.push(move);
+            },
+
+            invalid: function (move) {
+                if (move.player !== this.activePlayer) {
+                    return "wrong player";
+                }
+                if (move.start[0] >= this.sideLength
+                        || move.end[0] >= this.sideLength
+                        || move.start[1] >= this.sideLength
+                        || move.end[1] >= this.sideLength) {
+                    return "outside board";
+                }
+                if (this.occupant(move.start) !== move.player) {
+                    return "player's piece not at start";
+                }
+                if (this.occupant(move.end) !== types.none) {
+                    return "end position olready occuppied";
+                }
+                if (!move.horizontal && !move.vertical) {
+                    return "not straight";
+                }
+                if (!this.clear(move)) {
+                    return "path blocked";
+                }
+                return false;
+            }
+        };
+
+        return Board;
+    }());
+
+    root.TypeError = TypeError;
+    root.Move = Move;
+    root.Board = Board;
+    root.team = team;
+
+}(this));
--- test.js
+++ test.js
@@ -2,15 +2,12 @@
 "use strict";
 
 var tafl = require("./tafl"),
-    Board = tafl.Board,
-    board;
+    board = new tafl.Board(),
+    team = tafl.team;
 
-console.log(tafl);
-board = new Board();
-
-board.update({ player: "defenders", from: [3, 5], to: [2, 5] });
-board.update({ player: "attackers", from: [1, 4], to: [1, 1] });
-board.update({ player: "defenders", from: [5, 3], to: [2, 3] });
-board.update({ player: "attackers", from: [5, 1], to: [9, 1] });
+board.update({ player: team.defenders, from: [3, 5], to: [2, 5] });
+board.update({ player: team.attackers, from: [1, 4], to: [1, 1] });
+board.update({ player: team.defenders, from: [5, 3], to: [2, 3] });
+board.update({ player: team.attackers, from: [5, 1], to: [9, 1] });
 
 console.log(board.state);
