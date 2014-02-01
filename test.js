19be7cd1ed1f726fd1ea7b5f0e425835d0af8efd
Chris Bowdon <c.bowdon@bath.edu>
2014-02-01 17:48:59 +0000

    Added bad move test.

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 /*jslint browser:true, node: true */
 (function (exports) {
-  "use strict";
+    "use strict";
 
-  exports.test = function (board, team) {
+    exports.test = function (board, team) {
 
-    board.update({ player: team.defenders, from: [3, 5], to: [2, 5] });
-    board.update({ player: team.attackers, from: [1, 5], to: [1, 1] });
-    board.update({ player: team.defenders, from: [5, 3], to: [2, 3] });
-    board.update({ player: team.attackers, from: [5, 1], to: [9, 1] });
-    board.update({ player: team.defenders, from: [4, 7], to: [4, 10] });
+        board.update({ player: team.defenders, from: [3, 5], to: [2, 5] });
+        board.update({ player: team.attackers, from: [1, 5], to: [1, 1] });
+        board.update({ player: team.defenders, from: [5, 3], to: [2, 3] });
+        board.update({ player: team.attackers, from: [5, 1], to: [9, 1] });
+        // A fatal error
+        board.update({ player: team.defenders, from: [2, 3], to: [2, 1] });
+        // Attacker takes defender
+        board.update({ player: team.attackers, from: [3, 0], to: [3, 1] });
 
-    console.log(board.state);
-  };
+        console.log(board.toString());
+    };
 
 }(this));
